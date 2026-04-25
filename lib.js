58fc1cb84ec016a2f5eabdec716f979c1a702452
Wissididom <30803034+Wissididom@users.noreply.github.com>
2026-04-25 14:11:18 +0200

    Add 2026 to array

--- lib.js
+++ lib.js
@@ -8,7 +8,7 @@ const PROGRAM_FILES = [process.env['ProgramFiles(x86)'], process.env['ProgramFil
 
 
 const EDITIONS = ['Enterprise', 'Professional', 'Community', 'BuildTools']
-const YEARS = ['2022', '2019', '2017']
+const YEARS = ['2026', '2022', '2019', '2017']
 
 const VsYearVersion = {
     '2026': '18.0',
