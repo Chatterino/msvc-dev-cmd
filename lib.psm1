d7ac4d22f827c4881f820b10b8e742208b73eb12
Wissididom <30803034+Wissididom@users.noreply.github.com>
2026-07-11 22:43:26 +0200

    d

--- lib.psm1
+++ lib.psm1
@@ -274,7 +274,7 @@ function Initialize-MSVCDevCmd {
 			{
 				$value = Get-FilteredPathValue $value
 			}
-			Set-Item -Path "Env:$name" -Value $value
+			Set-Item -Path "Env:$name" -Value $value | Out-Null
 			Write-Host "$name=$value" # For testing
 			Add-Content -Path $env:GITHUB_ENV -Value "$name=$value" -Encoding utf8
 		}
